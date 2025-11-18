c9e52310b088b36f4e3695d4f325e5f605925445
Meti00a <muhamedhajra00@gmail.com>
2025-11-18 20:22:36 +0100

    Added a list of the rooms and also the availability. So that the Secretary can check and view the available rooms.

--- Program.cs
+++ Program.cs
@@ -1,29 +1,42 @@
-﻿
-using System.Collections;
-using System.Reflection.Metadata;
+﻿using System.Collections;
 using App;
 
 
-    string filepath = "users.csv";
-    string roomsFile = "rooms.csv";
+string filepath = "users.csv";   //en filpath till Users.csv
+string roomsFile = "rooms.csv"; //en filpath till rooms.csv
+
 bool running = true;
+List<Room> rooms = new List<Room>                                                       //Lista av rooms som är tillgängliga
+{
+    new Room("G6", RoomStateEnum.Available),
+    new Room("6E", RoomStateEnum.Available),
+    new Room("A4", RoomStateEnum.Available),
+    new Room("L2", RoomStateEnum.Available),
+    new Room("G20", RoomStateEnum.Available),
+    new Room("4G5", RoomStateEnum.Available),
+    new Room("8D", RoomStateEnum.Available),
+    new Room("Z17", RoomStateEnum.Available),
+};
+
+
+
 
 while (running)
 {
     Console.Clear();
 
     {
-        Console.WriteLine("Username: ");
+        Console.WriteLine("Username: ");            //Inlogging för användarnamn
         string username = Console.ReadLine();
         Console.Clear();
 
-        Console.WriteLine("Password: ");
+        Console.WriteLine("Password: ");        //Inlogging för lösenord   
         string password = Console.ReadLine();
         Console.Clear();
-        
 
-    
-     Console.WriteLine("Welcome to hotel plaza!");
+
+
+        Console.WriteLine("Welcome to hotel plaza!");   //Välkommen text med val.
 
         {
 
@@ -35,26 +48,36 @@ while (running)
             Console.WriteLine("[4] quit");
             Console.Write("Choose options: ");
 
-            string val = Console.ReadLine();        //string to that enables the secretary to choose from
+            string val = Console.ReadLine();        // en string som läser in användarens val
 
 
-            switch (val)            //options to choose from
+            switch (val)            //val att välja mellan
             {
                 case "1":
-                    Console.WriteLine("Available rooms: 4D, 6G, 9E");
+                    Console.WriteLine("Available Rooms:");                      //en foreach loop som går igenom rummen och skriver ut de tillgängliga.
+                    foreach (Room room in rooms)
+                    {
+                        if (room.RoomState == RoomStateEnum.Available)                          //en if sats som checkar ifall rummet är tillgänglig.
+                        {
+                            Console.WriteLine($"Room {room.RoomNumber} is {room.RoomState}");
+                        }
+                    }
+
                     Console.ReadLine();
                     break;
 
 
                 case "2":
-                    Console.WriteLine("Choose which room you would like to book: 4D, 6E, 9E");
+                    Console.WriteLine("Choose which room you would like to book:");  //en case för en bokningsfunktion av rum.
+                    Console.ReadLine();
                     break;
 
                 case "3":
-                    Console.WriteLine("Manage rooms");
+                    Console.WriteLine("Manage rooms");     //en case för att hantera vilka rum som är bokade eller tillgängliga.
+                    Console.ReadLine();
                     break;
                 case "4":
-                    running = false;
+                    running = false;                //en case för att avsluta programmet.
                     break;
             }
         }
