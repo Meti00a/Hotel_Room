8bfb57ad470587bf1ff08fb271587826ad45b749
Meti00a <muhamedhajra00@gmail.com>
2025-12-20 01:50:47 +0100

    Implemented a minor fix with the cs file (it was missing a guest name string) in order for the program to function properly.

--- Room.cs
+++ Room.cs
@@ -8,15 +8,17 @@ public enum RoomStateEnum
 
 }
 
-class Room
+class Room                      //en klass för rummen med status, string för rumsnummer och ifall rummen är tillgänglig eller inte.
 {
     public string RoomNumber;
     public RoomStateEnum RoomState;
+    public string? GuestName;
+    public Room(string roomNumber, RoomStateEnum roomState = RoomStateEnum.Available)
     
-    public Room(string roomNumber, RoomStateEnum roomState = RoomStateEnum.Available) 
+
     {
         RoomNumber = roomNumber;
         RoomState = roomState;
         
     }
-}
+}  
